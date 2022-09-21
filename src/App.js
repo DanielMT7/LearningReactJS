c225825224618acf33dd339f256b2544da7523e9
Daniel Motta <danielmotta04@hotmail.com>
2022-09-21 18:57:05 -0300

    Mudando estrutura, aprendendo renderização condicional

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import './App.css'
-import HelloWorld from './components/HelloWorld'
-import Pessoa from './components/Pessoa'
-import SayMyName from './components/SayMyName'
-import List from './components/List'
-import Evento from './components/Evento'
-import Form from './components/Form'
+import Condicional from './components/Condicional'
 
 function App() {
   return (
     <div className="App">
-      <h1>Testando Eventos</h1>
-      <Evento numero="1" />
-      <Form />
+      <h1>Renderização Condicional</h1>
+      <Condicional />
     </div>
   )
 }
